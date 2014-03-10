ad2eefa3e70205f110cd1eb2737e10c4d755e3c9
krishnagniyes <krishnagniyes@gmail.com>
2014-03-10 23:10:59 +0530

    Added fields for attendee details

--- iCheckInGenie/SharedAppKit/Data Structures/AttendeeDetail.h
+++ iCheckInGenie/SharedAppKit/Data Structures/AttendeeDetail.h
@@ -2,16 +2,36 @@
 //  AttendeeDetail.h
 //  iCheckInGenie
 //
-//  Created by Krishna on 02/03/14.
+//  Created by Krishna on 10/03/14.
 //  Copyright (c) 2014 Apple. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
+
 @interface AttendeeDetail : NSManagedObject
-@property (nonatomic, copy) NSString *name;
-@property (nonatomic, copy) NSString *deviceType;
-@property (nonatomic, copy) NSString *email;
-@property (nonatomic, assign) BOOL isUpdatedR;
+
+@property (nonatomic, retain) NSString * attendeeType;
+@property (nonatomic, retain) NSString * prefix;
+@property (nonatomic, retain) NSString * firstName;
+@property (nonatomic, retain) NSString * lastName;
+@property (nonatomic, retain) NSString * workSpeciality;
+@property (nonatomic, retain) NSString * groupAffiliation;
+@property (nonatomic, retain) NSString * numberofYearsinpractice;
+@property (nonatomic, retain) NSString * degree;
+@property (nonatomic, retain) NSString * yearGraduated;
+@property (nonatomic, retain) NSString * pri_Address1;
+@property (nonatomic, retain) NSString * pri_Address2;
+@property (nonatomic, retain) NSString * city;
+@property (nonatomic, retain) NSString * state;
+@property (nonatomic, retain) NSString * zipcode;
+@property (nonatomic, retain) NSString * email;
+@property (nonatomic, retain) NSString * telephone;
+@property (nonatomic, retain) NSString * cell;
+@property (nonatomic, retain) NSString * workPhone;
+@property (nonatomic, retain) NSString * dietPreference;
+@property (nonatomic, retain) NSString * tables;
+@property (nonatomic, retain) NSString * alumniChapter;
 
 @end
--- iCheckInGenie/SharedAppKit/Data Structures/AttendeeDetail.m
+++ iCheckInGenie/SharedAppKit/Data Structures/AttendeeDetail.m
@@ -2,12 +2,35 @@
 //  AttendeeDetail.m
 //  iCheckInGenie
 //
-//  Created by Krishna on 02/03/14.
+//  Created by Krishna on 10/03/14.
 //  Copyright (c) 2014 Apple. All rights reserved.
 //
 
 #import "AttendeeDetail.h"
 
+
 @implementation AttendeeDetail
+
+@dynamic attendeeType;
+@dynamic prefix;
+@dynamic firstName;
+@dynamic lastName;
+@dynamic workSpeciality;
+@dynamic groupAffiliation;
+@dynamic numberofYearsinpractice;
+@dynamic degree;
+@dynamic yearGraduated;
+@dynamic pri_Address1;
+@dynamic pri_Address2;
+@dynamic city;
+@dynamic state;
+@dynamic zipcode;
+@dynamic email;
+@dynamic telephone;
+@dynamic cell;
+@dynamic workPhone;
+@dynamic dietPreference;
+@dynamic tables;
+@dynamic alumniChapter;
 
 @end
