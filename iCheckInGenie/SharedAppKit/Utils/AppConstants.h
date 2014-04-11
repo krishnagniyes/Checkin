48f397d30132653669dc815aba677d372cb3333a
krishnagniyes <krishnagniyes@gmail.com>
2014-04-11 09:22:17 +0530

    Added image file

--- iCheckInGenie/SharedAppKit/Utils/AppConstants.h
+++ iCheckInGenie/SharedAppKit/Utils/AppConstants.h
@@ -6,7 +6,7 @@
 #define kLogoutMessage @"The database will automatically sync before logging out"
 
 
-#define kBackgroundColor_iPhone @"felt.png"
+#define kBackgroundColor_iPhone @"MenuBackground.png"
 #define kBackgroundColor_iPad @"background.png"
 
 //[self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"background.png"]]];
