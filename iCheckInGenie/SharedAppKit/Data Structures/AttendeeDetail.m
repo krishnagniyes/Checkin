2dc0845116ef5709243dda437c221bab65963907
krishnagniyes <krishnagniyes@gmail.com>
2014-05-14 09:42:49 +0530

    Removed header

--- iCheckInGenie/SharedAppKit/Data Structures/AttendeeDetail.m
+++ iCheckInGenie/SharedAppKit/Data Structures/AttendeeDetail.m
@@ -3,7 +3,7 @@
 //  iCheckInGenie
 //
 //  Created by Krishna on 10/03/14.
-//  Copyright (c) 2014 Apple. All rights reserved.
+//  Copyright (c) 2014 Gniyes. All rights reserved.
 //
 
 #import "AttendeeDetail.h"
