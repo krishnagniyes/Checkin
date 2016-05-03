825c1820bc6bd4ca07c4a72910a357502ae77ecc
Krishna <krishnagniyes@gmail.com>
2016-05-03 16:01:50 -0700

    Test Error

--- iCheckInGenie/SharedAppKit/Utils/PickerPopupDelegateProtocol.h
+++ iCheckInGenie/SharedAppKit/Utils/PickerPopupDelegateProtocol.h
@@ -7,14 +7,14 @@
 //
 
 
-@protocol PickerPopupDelegate
-- (void) pickerDoneButtonPressed:(id)sender withData:(id) data;
-- (void) pickerNextButtonPressed:(id)sender withData:(id) data;
-- (void) pickerDateChanged:(id)sender withData:(id) data;
-@optional
-- (void) pickerDoneButtonPressed:(id)sender withData:(id) data andSelectedIndex:(NSUInteger)selectdIndex;
-
-
-// Krishna
-@end
+//@protocol PickerPopupDelegate
+//- (void) pickerDoneButtonPressed:(id)sender withData:(id) data;
+//- (void) pickerNextButtonPressed:(id)sender withData:(id) data;
+//- (void) pickerDateChanged:(id)sender withData:(id) data;
+//@optional
+//- (void) pickerDoneButtonPressed:(id)sender withData:(id) data andSelectedIndex:(NSUInteger)selectdIndex;
+//
+//
+//// Krishna
+//@end
 
