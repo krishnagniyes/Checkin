dc229527496f0dee86a7dd30dd511664ad232532
Krishna <krishnagniyes@gmail.com>
2016-05-03 16:47:50 -0700

    Test Build Error --- Error

--- iCheckInGenie/SharedAppKit/Utils/PickerPopupDelegateProtocol.h
+++ iCheckInGenie/SharedAppKit/Utils/PickerPopupDelegateProtocol.h
@@ -14,6 +14,9 @@
 //@optional
 //- (void) pickerDoneButtonPressed:(id)sender withData:(id) data andSelectedIndex:(NSUInteger)selectdIndex;
 //
+
+srahie;goruwehiRPWEO:Q"K:ASDol ae8f;igso/j;k wo[m 0g8'acklnx;lm'k;
+["
 //
 //// Krishna
 //@end
