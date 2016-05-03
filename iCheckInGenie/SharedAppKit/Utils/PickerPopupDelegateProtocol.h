20a1e2a217567716fab568d5fffdf21aad67be6a
Krishna <krishnagniyes@gmail.com>
2016-05-03 16:00:16 -0700

    Test

--- iCheckInGenie/SharedAppKit/Utils/PickerPopupDelegateProtocol.h
+++ iCheckInGenie/SharedAppKit/Utils/PickerPopupDelegateProtocol.h
@@ -7,12 +7,14 @@
 //
 
 
-//@protocol PickerPopupDelegate
-//- (void) pickerDoneButtonPressed:(id)sender withData:(id) data;
-//- (void) pickerNextButtonPressed:(id)sender withData:(id) data;
-//- (void) pickerDateChanged:(id)sender withData:(id) data;
-//@optional
-//- (void) pickerDoneButtonPressed:(id)sender withData:(id) data andSelectedIndex:(NSUInteger)selectdIndex;
-//
-//@end
+@protocol PickerPopupDelegate
+- (void) pickerDoneButtonPressed:(id)sender withData:(id) data;
+- (void) pickerNextButtonPressed:(id)sender withData:(id) data;
+- (void) pickerDateChanged:(id)sender withData:(id) data;
+@optional
+- (void) pickerDoneButtonPressed:(id)sender withData:(id) data andSelectedIndex:(NSUInteger)selectdIndex;
+
+
+// Krishna
+@end
 
