c622243e90e5b57e61ad1a6d33d5e05f2c253df5
Krishna <krishnagniyes@gmail.com>
2016-05-03 17:15:18 -0700

    Problem fixed

--- iCheckInGenie/SharedAppKit/Utils/PickerPopupDelegateProtocol.h
+++ iCheckInGenie/SharedAppKit/Utils/PickerPopupDelegateProtocol.h
@@ -7,17 +7,12 @@
 //
 
 
-//@protocol PickerPopupDelegate
-//- (void) pickerDoneButtonPressed:(id)sender withData:(id) data;
-//- (void) pickerNextButtonPressed:(id)sender withData:(id) data;
-//- (void) pickerDateChanged:(id)sender withData:(id) data;
-//@optional
-//- (void) pickerDoneButtonPressed:(id)sender withData:(id) data andSelectedIndex:(NSUInteger)selectdIndex;
-//
+@protocol PickerPopupDelegate
+- (void) pickerDoneButtonPressed:(id)sender withData:(id) data;
+- (void) pickerNextButtonPressed:(id)sender withData:(id) data;
+- (void) pickerDateChanged:(id)sender withData:(id) data;
+@optional
+- (void) pickerDoneButtonPressed:(id)sender withData:(id) data andSelectedIndex:(NSUInteger)selectdIndex;
 
-srahie;goruwehiRPWEO:Q"K:ASDol ae8f;igso/j;k wo[m 0g8'acklnx;lm'k;
-["
-//
-//// Krishna
-//@end
+@end
 
